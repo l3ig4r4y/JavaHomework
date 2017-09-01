e1965953750a32b06d742426be34b3bc1c883ba9
l3ig4r4y <garay.lei@gmail.com>
2017-09-01 18:30:26 -0400

    Update Activity03.java

--- CoreJava_Assignment/Activity03.java
+++ CoreJava_Assignment/Activity03.java
@@ -9,30 +9,30 @@ public class Activity03
 {
 	public static void main(String[] args) 
 	{
-		Activity03 demo = new Activity03(); 						//instance of Activity03 class created in main class
-	}
+		Activity03 demo = new Activity03(); 				// instance of Activity03 class created 
+	}									// in main class
 	
 	Activity03()
 	{
-																	//brief description of what program does
+										// brief description of what program does
 		System.out.println
 		("This program prints EVEN and ODD numbers from a given RANGE using multithreading and user input");
 		System.out.println
 		("To determine the range we need a start and and end number\n");
 		
-		Scanner sc 		= new Scanner(System.in);					// declare and initializes scanner(input reader)
+		Scanner sc 		= new Scanner(System.in);		// declare and initializes scanner(input reader)
 		System.out.print("Enter range START number:\t");
-		int 	start	= sc.nextInt();								// retrieves start of range from user
+		int 	start	= sc.nextInt();					// retrieves start of range from user
 		System.out.print("Enter range END   number:\t");
-		int 	end		= sc.nextInt();								// retrieves end   of range from user
+		int 	end		= sc.nextInt();				// retrieves end   of range from user
 		sc.close();
 		Number_Program a= new Number_Program(start,end,"ODD");		// creates class object for ODD  #s
-		Thread t01 		= new Thread(a);							// creates thread for this object
-		t01.start();												// it starts thread
+		Thread t01 		= new Thread(a);			// creates thread for this object
+		t01.start();							// it starts thread
 		
 		Number_Program b= new Number_Program(start,end,"EVEN");		// creates class objects or EVEN #s
-		Thread t02 		= new Thread(b);							// following steps are the same as above but
-		t02.start();												// with corresponding object b and thread t02
+		Thread t02 		= new Thread(b);			// following steps are the same as above but
+		t02.start();						        // with corresponding object b and thread t02
 	}
 }
 
@@ -47,15 +47,15 @@ class Number_Program implements Runnable
 		
 		Number_Program(int start, int end, String numberType)
 		{
-			Number_Program.start 		= start;					// declares and store input from constructor
-			Number_Program.end			= end;						// and assigns it to class variables
+			Number_Program.start 		= start;		// declares and store input from constructor
+			Number_Program.end			= end;		// and assigns it to class variables
 			this.numberType 			= numberType;
 			this.numberList				= new ArrayList<Integer>();
 		}
-		public void start() 										// starts thread
+		public void start() 						// starts thread
 		{}
 		@Override
-		public void run() 											// calls print method based on eve or odd
+		public void run() 						// calls print method based on eve or odd
 		{															// stores values in an arraylist
 			if		(numberType.equalsIgnoreCase("odd"))
 			{
@@ -74,7 +74,7 @@ class Number_Program implements Runnable
 				numberList = new ArrayList<Integer>();
 		}	
 		
-		void print(int start, int end, int remainder)				// prints values in loop range based on
+		void print(int start, int end, int remainder)			// prints values in loop range based on
 		{ 															// remainder value. 1 for odd, 2 for even.
 			for(int i=start; i<=end; i++)
 			{	
