2496bd43eff236e74094a78ead7e43312901d6bd
l3ig4r4y <garay.lei@gmail.com>
2017-09-01 18:16:27 -0400

    Rearranged comments
    Trimmed, aligned and modify comments for better reading

--- CoreJava_Assignment/Activity01.java
+++ CoreJava_Assignment/Activity01.java
@@ -5,89 +5,79 @@ import java.util.Scanner;
 
 class Activity01 
 {	
-	//Static variables for scanner, n (length of array), array, index and element of array//
-	static Scanner sc = new Scanner(System.in);
-	static int n = 0;
-	static int [] arr = new int[n];
-	static int index = 0;
-	static int element = 0;
+									
+	static Scanner sc = new Scanner(System.in);			// Static variables for scanner, 
+	static int n = 0;						// n (length of array)
+	static int [] arr = new int[n];					// array object with n length 
+	static int index = 0;						// index variable
+	static int element = 0;						// integer variable that will hold the pulled element
 	
 	Activity01()
 	{
-		//brief description of the program
+									//brief description of the program
 		System.out.println
 		(
 				"This program allows you to select the length of an array.\n"+
 				"Enter the elements of the array manually and finally ...\n"+
 				"Pull an element by Index number!\n"
 		);
-		//class variables/objects made from return methods
+									// class variables/objects made from return methods
 		n 	= getArrayLength(); 				// array length retrieved from user
 		arr	= setArrayElements(n); 				// array populated by user
-		index = getIndex();						// element index retrieved by user
-		element = getElementByIndex(arr,index);	// element selected by index printed unless exception caught
-	}
+		index = getIndex();					// element index retrieved by user
+		element = getElementByIndex(arr,index);	                // element selected by index printed 
+	}								// unless exception is caught	
 	
 	
-	//this method asks user to enter length or array and returns that number using SCANNER(SYSTEM.IN)
-	static int 		getArrayLength()
-	{		
+									
+									
+	static int 		getArrayLength()			// this method asks user to enter length of array
+	{								// and returns that number using Scanne class
 		System.out.println("Enter number of elements in the array: ");
 		int arrlength = sc.nextInt();
 		return arrlength;
-	}
-	
-	
-	//this method takes the length of an array provided by the user to create the array using FOR LOOP
-	// and asks user to manually enter the values for each element of the array
-	// finally the array is returned in the form of int[] array
-	static int[] 		setArrayElements(int n)
-	{
-		int[] arr = new int[n]; //initializes array and length based on previous method
-		for (int i = 0; i < arr.length; i++) 
+	}								
+								
+	static int[] 		setArrayElements(int n)			// method asks user for elements and returns array	
+	{								
+		int[] arr = new int[n]; 				// initializes array and length 
+		for (int i = 0; i < arr.length; i++) 			// based on previous input
 		{
-			//prompts user to enter elements manually one at a time using a FOR LOOP
-			System.out.print("Enter element "+(i+1)+": ");
-			arr[i]=sc.nextInt(); //stores user input in array by index
+									// prompts user to enter elements
+			System.out.print("Enter element "+(i+1)+": ");	// manually one at a time using a FOR LOOP
+			arr[i]=sc.nextInt(); 				// stores user input in array by index
 		}
-		return arr; //returns array with values provided by user
+		return arr; 						// returns array with values provided by user
 	}
-	
-	
-	//this method ask user to enter the index of the element he/she wishes to pull from the array
-	static int 		getIndex()
-	{
-		System.out.println("Enter the index of the array element you want to access");
-		//prompts user to provide the desired index
-		index = sc.nextInt();
-		//stores user input
+						
+	static int 		getIndex()				// this method asks user to enter the index
+	{								// of the element to be to pulled from the array
+		System.out.println					// prompts user to provide the desired index
+		("Enter the index of the array element you want to access");
+		
+		index = sc.nextInt();					// stores user input into index variable
+		
 		return index;
-	}
+	}								
 	
-	
-	//this method prints the array element as per the provided index or informs the user that such
-	//index goes over the length of the array, making it "out of range"
-	static int 		getElementByIndex(int[] arr, int index)
-	{
-		try
+	static int 		getElementByIndex(int[] arr, int index)	// this method prints the array element as per
+	{								// the provided index if index goes over the length
+		try							// throws outofrange exception
 		{		
 				System.out.println("The array element at index "+index+" is "+arr[index]);
 				System.out.println("The array element successfully accessed");
-				return arr[index];
-			
+				return arr[index];	
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			System.out.println("IndexOutOfRangeException\n"); //custom message for outofbounds exception
+			System.out.println("IndexOutOfRangeException\n");// custom message for outofbounds exception
 			
 		}
 		return 0;
 	}
 	
-	
 	public static void main(String[] args) 
 	{
-		Activity01 demo = new Activity01();//instance of Activity01 class created in main class
+		Activity01 demo = new Activity01();			// instance of Activity01 class created in main class
 	}
-	
 }
